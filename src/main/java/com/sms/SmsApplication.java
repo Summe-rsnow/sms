4edf3ff54e25d20533d81bc6a47f99097cc9ca9e
sssnow <1543297674@qq.com>
2024-01-16 11:28:41 +0800

    修正sql文件

--- src/main/java/com/sms/SmsApplication.java
+++ src/main/java/com/sms/SmsApplication.java
@@ -2,8 +2,9 @@ package com.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmsApplication.class, args);
